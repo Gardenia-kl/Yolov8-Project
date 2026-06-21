f11d71e9ec9af03539f28fb69df0a6d8ed238fcc
lm <lm135749@163.com>
2026-06-21 14:46:50 +0800

    调用硬件ISP来读取摄像头，排除摄像头读取故障

--- rasp_ncnn.py
+++ rasp_ncnn.py
@@ -1,49 +1,46 @@
 import cv2
 import time
 from ultralytics import YOLO
+from picamera2 import Picamera2
 
 
 def main():
-    model_path = "/home/lm/best_ncnn_model"
-    print(f"loading ncnn_model: {model_path}")
-
+    model_path = "/home/lm/herbal_project/best_ncnn_model"
+    print(f"Loading NCNN model: {model_path}")
     model = YOLO(model_path, task='detect')
 
-    cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
-    if not cap.isOpened():
-        print("error: Cannot open camera")
-        return
+    print("Initializing Picamera2 for IMX219...")
+    picam2 = Picamera2()
+    config = picam2.create_preview_configuration(main={"size": (640, 480), "format": "RGB888"})
+    picam2.configure(config)
+    picam2.start()
 
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-
-    print("Start inference implementation.....press Q to quit")
-
+    print("Inference started. Press 'Q' to quit.")
     prev_time = 0
+
     while True:
-        success, frame = cap.read()
-        if not success:
-            print("can't get the frame")
+        try:
+            frame_raw = picam2.capture_array()
+            frame = frame_raw
+
+        except Exception as e:
+            print(f"Failed to grab frame: {e}")
             break
 
-
-        results = model.predict(source=frame, save=False, show=False, conf=0.5)
-
+        results = model.predict(source=frame, save=False, show=False, conf=0.3)
         annotated_frame = results[0].plot()
 
         current_time = time.time()
-        # 补齐了右括号
         fps = 1 / (current_time - prev_time)
         prev_time = current_time
 
         cv2.putText(annotated_frame, f"FPS: {fps:.1f}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
         cv2.imshow("Herbal Medicine Detection", annotated_frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-    cap.release()
+    picam2.stop()
     cv2.destroyAllWindows()
 
 
